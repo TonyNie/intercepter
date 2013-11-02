3350696a2e00f4878125b4bf29883a3741f5aaf4
tony_nie <cloudnie@gmail.com>
2013-11-02 13:54:24 +0800

    UI: add tabhost and fragment
    Signed-off-by: tony_nie <cloudnie@gmail.com>

---
+++ src/com/tony_nie/intercepter/BLFragment.java
@@ -0,0 +1,14 @@
+package com.tony_nie.intercepter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BLFragment extends Fragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.layout_blacklist, container, false);
+	}
+}
--- src/com/tony_nie/intercepter/MainActivity.java
+++ src/com/tony_nie/intercepter/MainActivity.java
@@ -1,15 +1,19 @@
 package com.tony_nie.intercepter;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.Window;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	public static final String CONFIG_PATH = "intercepter.xml";
 	public static final String CONFIG_NAME = "intercepter_config";
 	public static final String INTERCCPTER = "intercepter";
 	private SPConfig spConfig;
+	private TabHost tabHost;
+	private static int currentlayout = 0;
 
 	private void printSPconfig() {
 		String list[] = spConfig.getBlacklist();
@@ -35,7 +39,6 @@ public class MainActivity extends Activity {
 
 		spConfig.addNumber2Blacklist("" + phone++);
 		spConfig.addNumber2Blacklist("" + phone++);
-		
 
 		spConfig.addNumber2WhiteList("" + phone++);
 		spConfig.addNumber2WhiteList("" + phone++);
@@ -46,23 +49,28 @@ public class MainActivity extends Activity {
 		printSPconfig();
 	}
 
-
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		setContentView(R.layout.activity_main);
 		Log.d("Activety", "Created.");
 		spConfig = new SPConfig(this, CONFIG_NAME);
 
-		/* long begin = System.currentTimeMillis();
-		PhoneStatReceiver.inContacts(this, "15850053917");
-		PhoneStatReceiver.inContacts(this, "15850053721");
-		long end = System.currentTimeMillis();
-		Log.d(INTERCCPTER, "Query All cantacts: " + (end - begin)); */
-		
-		//testSPconfig();
+		/*
+		 * long begin = System.currentTimeMillis();
+		 * PhoneStatReceiver.inContacts(this, "15850053917");
+		 * PhoneStatReceiver.inContacts(this, "15850053721"); long end =
+		 * System.currentTimeMillis(); Log.d(INTERCCPTER, "Query All cantacts: "
+		 * + (end - begin));
+		 */
+
+		// testSPconfig();
+
+		setFragment();
+		changeLayout();
 	}
 
 	@Override
@@ -70,6 +78,23 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private void changeLayout() {
+
+		tabHost.setCurrentTab(currentlayout);
+	}
+
+	private void setFragment() {
+		tabHost = (TabHost) findViewById(R.id.tabhost);
+		tabHost.setup();
+
+		tabHost.addTab(tabHost.newTabSpec("blacklist").setIndicator("blacklist").setContent(R.id.frag_blacklist));
+		tabHost.addTab(tabHost.newTabSpec("whitelist").setIndicator("whitelist").setContent(R.id.frag_whitelist));
+		tabHost.addTab(tabHost.newTabSpec("setting").setIndicator("setting").setContent(R.id.frag_setting));
+		
+
+		tabHost.setCurrentTab(0);
 	}
 
 }
